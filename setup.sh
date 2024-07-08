6e47687e8d45fce9ad4168c86ebe6ebbcd3cedb0
Jason Hiser <jdhiser@gmail.com>
2024-07-08 08:56:47 -0400

    Fixes for broken requirements

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 
 sudo apt update
-sudo apt install python3 python3-pip net-tools python-is-python3 python3-designateclient python3-neutronclient
+sudo apt install python3 python3-pip net-tools python-is-python3 python3-designateclient python3-neutronclient -y
 pip install pipreqs pyflakes
 pip install -r requirements.txt
 
